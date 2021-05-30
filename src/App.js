1beacce032d98dc98d5662f258672fe9d0824e7d
ravi gupta <ravi.gupta@happay.in>
2021-05-30 14:59:02 +0530

    initialize and completed the count

--- src/App.js
+++ src/App.js
@@ -1,25 +1,97 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useRef } from "react";
+import fetch from "node-fetch";
+import CounterText from "./Components/CounterText";
+import CounterLoader from "./Components/CounterLoader";
 
+import "./App.scss";
+
+const initialCounter = 1;
+const maxCount = 1000;
+const minCount = 1;
 function App() {
+  const [counter, setCounter] = useState(initialCounter);
+  const [loading, setLoading] = useState(true);
+  const isFirstRender = useRef(true);
+
+  console.log("APP", counter);
+
+  const updateCounter = async () => {
+    setLoading(true);
+    let response = await fetch(
+      "https://interview-8e4c5-default-rtdb.firebaseio.com/front-end/counter1.json",
+      {
+        method: "PUT",
+        body: JSON.stringify({ counter1: Number(counter) }),
+        headers: { "Content-Type": "application/json" },
+      },
+    );
+    let json = await response.json();
+    setCounter(json.counter1);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    async function fetchCount() {
+      let response = await fetch(
+        "https://interview-8e4c5-default-rtdb.firebaseio.com/front-end/counter1.json",
+      );
+      let json = await response.json();
+      setCounter(json.counter1);
+      setLoading(false);
+    }
+    fetchCount();
+  }, []);
+
+  useEffect(() => {
+    if (isFirstRender.current) {
+      isFirstRender.current = false; // toggle flag after first render/mounting
+      return;
+    }
+    if (!loading) {
+      updateCounter();
+    }
+  }, [counter]);
+
+  const handleChange = (event) => {
+    if (!(event.target.value > maxCount)) {
+      setCounter(event.target.value);
+    }
+  };
+
+  const increment = () => {
+    if (counter < maxCount && counter) {
+      setCounter(counter + 1);
+    }
+  };
+
+  const decrement = () => {
+    if (minCount < counter && counter) {
+      setCounter(counter - 1);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <CounterLoader loading={loading}></CounterLoader>
+      <div className="counterContainer">
+        <button className="decrement" onClick={decrement}>
+          <span>-</span>
+        </button>
+        <input
+          type="text"
+          name="counter"
+          id="counter"
+          value={counter}
+          onChange={handleChange}
+          size="1"
+        />
+        <button className="increment" onClick={increment}>
+          <span>+</span>
+        </button>
+      </div>
+      <CounterText counter={counter}></CounterText>
+    </>
   );
 }
 
-export default App;
+export default React.memo(App);
